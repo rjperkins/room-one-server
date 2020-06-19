6a2a70d1f67ad765f1cce426d1bf704326c6a1fa
Richard Perkins <richardjperkins89@gmail.com>
2020-06-19 21:49:08 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors')();
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = require('./router.js');
+
+app.use(cors)
+app.use(bodyParser.json());
+app.use(router);
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
