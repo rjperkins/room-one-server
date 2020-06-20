f12a843a7ef09f4cfe2acea74ff1fcab9a94560e
Richard Perkins <richardjperkins89@gmail.com>
2020-06-20 03:28:33 +0200

    fix: removed recursive attempt to fetch tracks

--- controllers/controller.recordLabel.js
+++ controllers/controller.recordLabel.js
@@ -4,9 +4,5 @@ exports.getTrackPreviews = async (req, res) => {
   await needle.get(`https://${process.env.COUDINARY_PUBLIC_KEY}:${process.env.COUDINARY_PRIVATE_KEY}@api.cloudinary.com/v1_1/dnonz22ez/resources/video/tags/${req.params.dj}?max_results=50`, function (error, response) {
     if (!error && response.statusCode === 200)
       res.status(200).send(response.body)
-    if (!response) {
-      console.log('error');
-      fetchPreviews()
-    }
   });
 }
