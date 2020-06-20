9a08f7bf459883cd86b2b3cc5f15999cbbf91a65
Richard Perkins <richardjperkins89@gmail.com>
2020-06-20 03:05:58 +0200

    fix: added production env if statement for build

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 
 const router = require('./router.js');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
+
 app.use(cors({
   origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://room-one-client.herokuapp.com'],
   default: 'https://room-one-client.herokuapp.com',
