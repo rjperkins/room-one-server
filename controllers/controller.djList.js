26c26264316664567e31dcb236eec0023379c9f8
Richard Perkins <richardjperkins89@gmail.com>
2020-08-07 20:09:56 +0200

    upadte cors for galleryURLS

--- controllers/controller.djList.js
+++ controllers/controller.djList.js
@@ -31,9 +31,18 @@ exports.getdjList = async (req, res) => {
 
 }
 
-exports.getGalleryURLs = async (req, res) => {
-  await needle.get(`https://${process.env.COUDINARY_PUBLIC_KEY}:${process.env.COUDINARY_PRIVATE_KEY}@api.cloudinary.com/v1_1/dnonz22ez/resources/image?max_results=500`, function (error, response) {
-    if (!error && response.statusCode === 200)
-      res.status(200).send(response.body)
-  });
+exports.getGalleryURLs = async (_, res) => {
+
+  const options = {
+    headers: { 'Access-Control-Allow-Origin': 'http://www.room1official.com' }
+  }
+
+  try {
+    await needle.get(`https://${process.env.COUDINARY_PUBLIC_KEY}:${process.env.COUDINARY_PRIVATE_KEY}@api.cloudinary.com/v1_1/dnonz22ez/resources/image?max_results=500`, options, function (error, response) {
+      if (!error && response.statusCode === 200)
+        res.status(200).send(response.body)
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
